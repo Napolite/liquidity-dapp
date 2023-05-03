2f1ee933581860d72a224de795ea497f060b4da3
Constantino <Okonemmanuelmma@gmail.com>
2023-05-04 00:14:13 +0100

    fixes : fixed "Dynamic require of path is not supported" error

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Homepage() {
-  return <div>homepage</div>;
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
 }
 
 export default Homepage;
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 
-const path = require("path")
 export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
