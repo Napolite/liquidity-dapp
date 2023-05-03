2507475dd0c58cf90e15e0241cd31a1f65a4bbb6
Constantino <Okonemmanuelmma@gmail.com>
2023-05-04 00:06:14 +0100

    #fixes : absolute import configs

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 const path = require("path")
 export default defineConfig({
   resolve: {
-    alias: {
-      components: path.resolve('src/components'),
-    },
-  },
+    alias: [{ find: '@', replacement: '/src' }],
+   },
   plugins: [react()],
 })
