f10cbc7f7977459485900f0d3d56d0c9803068ec
Constantino <Okonemmanuelmma@gmail.com>
2023-05-04 22:29:16 +0100

    #feature separating tabs

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./homepage.css";
 function Homepage() {
+  const [activeTab, setTab] = useState<any>("swap");
   return (
     <div className="homepage-root">
       <div className="homepage">
         <div className="tabs">
-          <div>Swap</div>
-          <div>Liquidity</div>
+          <div
+            className={`${activeTab === "swap" && "activeTab"}`}
+            onClick={() => setTab("swap")}
+          >
+            Swap
+          </div>
+          <div
+            className={`${activeTab === "Pool" && "activeTab"}`}
+            onClick={() => setTab("Pool")}
+          >
+            Liquidity
+          </div>
         </div>
       </div>
     </div>
