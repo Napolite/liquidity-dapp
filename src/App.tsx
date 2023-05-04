468da9710cbd4e120694e1c17f83674c0854e07a
Constantino <Okonemmanuelmma@gmail.com>
2023-05-04 13:00:39 +0100

    #feature : connect wallet function

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Homepage from "@/components/homepage";
 
 import "@/App.css";
 
+import { connectWallet } from "./actions";
+
 function Main() {
+  const [account, setAccount] = useState(null);
+
+  const connect = async () => {
+    account === null ? setAccount(await connectWallet()) : null;
+  };
   return (
     <div className="app-root">
       <div className="nav-bar">
-        <div>Connect Wallet</div>
+        <div onClick={connect}>
+          {account === null
+            ? "Connect Wallet"
+            : `${account[0].substring(0, 4)}...${account[0].substring(38, 42)}`}
+        </div>
       </div>
       <Homepage />
     </div>
---
+++ src/actions/connectWallet/index.ts
@@ -0,0 +1,9 @@
+const connectWallet = async () => {
+    let accounts:any;
+    const { ethereum } = window;
+  
+    return ethereum.request({ method: "eth_requestAccounts" }).then((res: any) => res);
+  
+  };
+  
+  export default connectWallet;
