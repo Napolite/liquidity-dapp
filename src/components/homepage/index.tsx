310c8f2bcaa3491c537d2c9ecfbdd7f75680d7b5
Constantino <Okonemmanuelmma@gmail.com>
2023-05-05 00:15:34 +0100

    #component : wirework for swap

--- src/components/homepage/index.tsx
+++ src/components/homepage/index.tsx
@@ -19,6 +19,29 @@ function Homepage() {
             Liquidity
           </div>
         </div>
+        {activeTab === "swap" && (
+          <div>
+            <div>
+              <div>You sell</div>
+              <div>Balance : 0</div>
+            </div>
+            <div>
+              <div>
+                <select name="tokens" id="">
+                  <option value="Napolite">Nap</option>
+                  <option value="Constantinite">Cons</option>
+                </select>
+              </div>
+              <div>
+                <input type="text" name="amount" />
+              </div>
+            </div>
+            <div>
+              <div>Napolite</div>
+              <div>no price</div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
